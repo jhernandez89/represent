ff3ee0ac9c4adeae52fcdf39b4278b857a50df38
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-08-07 17:25:27 -0600

    contact added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LandingPhotoComponent } from './landing-photo/landing-photo.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPhotoComponent,
     ProjectsComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
